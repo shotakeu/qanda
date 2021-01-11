4d13c01fe8400ee22a31144a869fc9c9173653bc
Shotaro Takeuchi <taro20090819@yahoo.co.jp>
2021-01-11 19:48:27 +0900

    lecture 120 end

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,21 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
-
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save error!'
+      render :new
+    end
+  end
   def edit
   end
+
+  private
+    def question_params
+      byebug
+      params.require(:question).permit(:name, :title, :content)
+    end
 end
